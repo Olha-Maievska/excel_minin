55703120cd5e1b3746a90f46c01d1718137fb93e
Olha Maievska <olhamaievska91@gmail.com>
2021-07-13 14:39:09 +0300

    Finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        configFile: "./babel.config.js"
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    extends: ["eslint:recommended", "google"],
+    rules: {
+        'semi': 'off',
+        'comma-dangle': 'off',
+        'require-jsdoc': 'off',
+        'no-trailing-spaces': 'off',
+        'indent': 'off',
+        'quotes': 'off',
+        'linebreak-style': 'off',
+        'eol-last': 'off'
+    }
+}
